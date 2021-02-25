9a9cf31703ccd0f17d4897c5b2f696bfba033c51
Peter.Dulin <petertdulin@gmail.com>
2021-02-25 13:33:27 -0500

    edits to js, got local storage working, still working on css changes with moment time

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,35 +2,88 @@
 var currentDate = moment().format("MMM Do YY");
 $("#currentDay").text(currentDate);
 
-$(document).ready(function () {
+function buttonSave() {
+    $(".saveBtn").on("click", function () {
+        var storedText = $(this).siblings(".user-data").val();
+        var storedTime = $(this).parent().attr("id");
 
-        //This function will set the time correctly with moment
-        function timeCheck() {
-            var currentTime = moment().format("HH");
+        localStorage.setItem(storedTime, storedText);
+    });
+}
 
-            $(".time-block").each(function () {
-                var taskArea = parseInt((this).attr("id").split("hour-block")[i]);
-                if (taskArea === currentTime) {
-                    $(this).addClass("present");
-                    $(this).removeClass("future");
-                    $(this).removeClass("past");
-                }
-                else if (taskArea < currentTime) {
-                    $(this).addClass("past");
-                    $(this).removeClass("future");
-                    $(this).removeClass("present");
-                }
-                else {
-                    $(this).addClass("future");
-                    $(this).removeClass("present");
-                    $(this).removeClass("past");
-                }
-            })
+
+//This function will set the time correctly with moment
+function timeCheck() {
+    var currentTime = moment().format("HH mm ss");
+    // console.log(currentTime);
+
+    //Possible loop for setting classes based on time currently not working
+    $(".time-block").each(function () {
+        var taskArea = parseInt($(this).attr("id").split("hour-block")[1]);
+        if (taskArea < currentTime) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
         }
+        else if (taskArea === currentTime) {
+            $(this).addClass("present");
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+        }
+        else {
+            $(this).addClass("future");
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+        }
+    })
+};
 
-        $(".saveBtn").on("click", function () {
-            var storedText = $(this).siblings
-        });
 
 
-})
+//How to get tasks from local storage
+$("#9-AM .user-data").val(localStorage.getItem("9-AM"));
+$("#10-AM .user-data").val(localStorage.getItem("10-AM"));
+$("#11-AM .user-data").val(localStorage.getItem("11-AM"));
+$("#12-PM .user-data").val(localStorage.getItem("12-PM"));
+$("#1-PM .user-data").val(localStorage.getItem("1-PM"));
+$("#2-PM .user-data").val(localStorage.getItem("2-PM"));
+$("#3-PM .user-data").val(localStorage.getItem("3-PM"));
+$("#4-PM .user-data").val(localStorage.getItem("4-PM"));
+$("#5-PM .user-data").val(localStorage.getItem("5-PM"));
+
+//Initial page function
+function init() {
+    timeCheck();
+    buttonSave();
+}
+
+$(document).ready(init());
+
+
+
+        //         for (var i=0; i< userTextArea.length; i++) {
+        //             if (userTextArea[i].getAttribute("value") > "hour-block"){
+        //                 userTextArea[i].addclass("future");
+
+        //             }else if (userTextArea[i].getAttribute("value") < "hour-block") {
+        //                 userTextArea[i].addclass("past");
+
+        //             }else {
+        //                 userTextArea[i].addclass("present");
+        //             }
+        //         }
+        //     });
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
